76960963536f4a643cc704c916588cdf22ff5b58
Renan Castro <renandf@gmail.com>
2022-01-23 09:15:53 +1100

    feat: Create foundation for Routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { readdirSync } from 'fs';
 const morgan = require('morgan');
 require('dotenv').config();
 
@@ -11,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// route
-app.get('/', (req, res) => {
-  res.send('you hit server endpoint')
-});
+// routes - import each route as a middleware from folder 'routes'
+readdirSync('./routes').map((r) =>
+  app.use('/api', require(`./routes/${r}`))
+);
 
 // port
 const port = process.env.PORT || 8000;
